fa81d809dcc0ec5db68dbe382e94eca9629f6f2c
p-w-g <p.grzelczyk@gmail.com>
2021-11-04 21:47:34 +0100

    refactor
    - remove redundant packages
    - add decent local serve
    - move js into single file (i know..)
    - update the base href which was bugprone
    - update readme

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import Task from './Task.mjs';
+class Task {
+  constructor(title, description, id) {
+    this.state = {
+      title,
+      description,
+      id,
+      done: false,
+    };
+  }
+}
 
 class List {
   constructor() {
@@ -37,7 +46,7 @@ class List {
     const itemElements = document.createElement('ul');
     itemElements.className = 'list-unstyled';
 
-    this.items.forEach(item => List.renderSingleListing(item, itemElements));
+    this.items.forEach((item) => List.renderSingleListing(item, itemElements));
 
     document.querySelector('#pending-list').innerHTML = '';
     document.querySelector('#pending-list').appendChild(itemElements);
@@ -61,4 +70,46 @@ class List {
   }
 }
 
-export default List;
+const footer = document.querySelector('footer');
+let ID = 0;
+
+footer.addEventListener('dblclick', () => {
+  footer.parentNode.removeChild(footer);
+});
+
+const list = new List();
+
+document.querySelector('#card-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const title = event.target[0].value;
+  if (title === '') {
+    return alert('Please add at least a Title');
+  }
+
+  const description = event.target[1].value;
+  list.addItem(title, description, ID);
+
+  document.querySelector('#card-title').value = '';
+  document.querySelector('#card-description').value = '';
+
+  ID += 1;
+});
+
+document.querySelector('#pending-list').addEventListener('click', (event) => {
+  if (event.target.nodeName === 'LI') {
+    return list.toggleDone(parseInt(event.target.id));
+  }
+  return list.toggleDone(parseInt(event.target.parentNode.id));
+});
+
+document
+  .querySelector('#pending-list')
+  .addEventListener('dblclick', (event) => {
+    if (event.target.nodeName === 'LI') {
+      list.removeItem(parseInt(event.target.id));
+    } else {
+      list.removeItem(parseInt(event.target.parentNode.id));
+    }
+    return list.renderList();
+  });
